7cce87c86b866c21187b8701b29e304d9178fed9
Aponysos <aponysos@163.com>
2017-11-04 23:56:55 +0800

    +TRACE_FUNC1, +TRACE_FUNC2

--- include/log4cpp/log4cpp.h
+++ include/log4cpp/log4cpp.h
@@ -32,3 +32,9 @@ private:
 }
 
 #define TRACE_FUNC() log4cpp::TraceFunction traceFunction_(__FUNCTION__)
+#define TRACE_FUNC1(param1) \
+  TRACE_FUNC(); \
+  DEBUG_LOG() << ##param1 << " = " << param1
+#define TRACE_FUNC2(param1, param2) \
+  TRACE_FUNC1(param1); \
+  DEBUG_LOG() << ##param2 << " = " << param2
